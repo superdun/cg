4a2574f440d9fe62c0758fe4bdeda1a3b275dc1b
li dun <475098936@qq.com>
2025-05-28 20:19:46 +0800

    init chapter8

---
+++ dx/d3d12book-master/MyDemos/Chapter8-shapes/Chapter8-shapes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "resource.h"
+#include "framework.h"
+#include "../Chapter4-Window/D3DApp.h"
+#include "../Chapter4-Window/GameTimer.h"
+#include <DirectXColors.h>
+#include "../Chapter6/MathHelper.h"
+#include "../Chapter6/UploadBuffer.h"
+#include "../Chapter7-shapes/FrameResource.h"
+#include "../Chapter7-shapes/GeometryGenerator.h"
