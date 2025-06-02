3c09205b101dcbff6c343b44f9c1fd98e97c19ff
li dun <475098936@qq.com>
2025-06-02 10:59:00 +0800

    移除方向光源，增加点光源支持
    在 `Chapter8-shapes.cpp` 中，通过循环动态生成 5 个点光源，简化光源管理并增强灵活性。同时，在 `Default.hlsl` 中将方向光源数量设置为 0，点光源数量增加到 10，以适应新的光照模型。

--- dx/d3d12book-master/MyDemos/Chapter8-shapes/Chapter8-shapes.cpp
+++ dx/d3d12book-master/MyDemos/Chapter8-shapes/Chapter8-shapes.cpp
@@ -308,12 +308,16 @@ void ShapesApp::UpdateMainPassCB(const GameTimer& gt)
 	mMainPassCB.TotalTime = gt.TotalTime();
 	mMainPassCB.DeltaTime = gt.DeltaTime();
 	mMainPassCB.AmbientLight = { 0.25f, 0.25f, 0.35f, 1.0f };
-	mMainPassCB.Lights[0].Direction = { 0.57735f, -0.57735f, 0.57735f };
-	mMainPassCB.Lights[0].Strength = { 0.6f, 0.6f, 0.6f };
-	mMainPassCB.Lights[1].Direction = { -0.57735f, -0.57735f, 0.57735f };
-	mMainPassCB.Lights[1].Strength = { 0.3f, 0.3f, 0.3f };
-	mMainPassCB.Lights[2].Direction = { 0.0f, -0.707f, -0.707f };
-	mMainPassCB.Lights[2].Strength = { 0.15f, 0.15f, 0.15f };
+    UINT k = 0;
+    for (int i = 0; i < 5; ++i, ++k) {
+
+        mMainPassCB.Lights[k].Position = { -5.0f, 3.5f, -10.0f + i * 5.0f };
+        mMainPassCB.Lights[k].Strength = { 0.6f, 0.6f, 0.6f };
+        k++;
+        mMainPassCB.Lights[k].Position = { +5.0f, 3.5f, -10.0f + i * 5.0f };
+        mMainPassCB.Lights[k].Strength = { 0.6f, 0.6f, 0.6f };
+    }
+
 
 	auto currPassCB = mCurrentFrameResource->PassCB.get();
 	currPassCB->CopyData(0, mMainPassCB);
